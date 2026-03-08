8ef57fa7e57126f8f6c68c0c802aa44729f28b50
Hasan Sabri <hsabri2007@gmail.com>
2026-03-08 12:59:23 +0000

    nice config

--- include/astgen.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "ast.h"
-#include "token.h"
-
-void ASTGenTree(ASTGlobal *global, TokenStream *stream);
--- src/main.c
+++ src/main.c
@@ -1,14 +1,6 @@
-#include "astgen.h"
-#include "token.h"
+#include <stdio.h>
 
 int main(void)
 {
-	TokenStream stream;
-	TokenStreamInit(&stream, "./tests/test1", 512);
-	TokenStreamNext(&stream);
-
-	ASTGlobal tree;
-	ASTGenTree(&tree, &stream);
-
-	return 0;
+	puts("hello");
 }
