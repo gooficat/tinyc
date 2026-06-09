07b98363cc6093ab7073e5c3230a2b85cc0dd6eb
Hasan Sabri <hsabri2007@gmail.com>
2026-06-09 16:22:51 +0100

    c23 time

--- src/main.c
+++ src/main.c
@@ -1,15 +1,6 @@
-#include "err.h"
-#include "gen.h"
-#include "lexer.h"
-#include "tree.h"
 #include <stdio.h>
 
-int main(void) {
-	puts("TinyC minimal C compiler");
-	lxr_init(fopen("test/4.c", "r"));
-	tree_parse();
-	tree_print();
-	codegen_init(fopen("bin/4.s", "w"));
-	codegen_tree();
+int main() {
+	fputs("Hello, World!", stdout);
 	return 0;
 }
